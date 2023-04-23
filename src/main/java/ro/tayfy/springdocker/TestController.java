bb49853596d7ee57914b1c1427d8f4fc182c5ba8
Tayfun Omer <tayfun94@gmail.com>
2023-04-23 21:58:48 +0300

    first commit

---
+++ src/main/java/ro/tayfy/springdocker/TestController.java
@@ -0,0 +1,13 @@
+package ro.tayfy.springdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "Sal";
+    }
+}
